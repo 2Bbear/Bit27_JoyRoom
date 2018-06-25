f6c622fefd7184231a71246b6ac2912c7e81fcd4
leesh93 <dltjdgnsok1@naver.com>
2018-06-25 22:02:06 +0900

    DB 함수추가
    DB 함수추가 아마 내가 디비 꺼놓으면 서버 막혀서 안될 수도??

--- CamProgram/HowToWebServer_0621/HowToWebServer_0621/HowToWebServer_0621.py
+++ CamProgram/HowToWebServer_0621/HowToWebServer_0621/HowToWebServer_0621.py
@@ -6,6 +6,11 @@ from flask import Flask, render_template,url_for, Response
 import face_recog
 import time
 import cv2
+import pymysql
+import mysql.connector
+from operator import eq
+import threading
+
 
 app = Flask(__name__)
 #변화 가능한 변수
@@ -13,6 +18,9 @@ rectangleColor=(0,0,255)
 foundedName='UnKnown'
 videoLocalTime=time.localtime()
 isFindPerson=False
+
+#DB에 쓰는 변수
+datalog = "nothing"
 
 #영상 전송===========================
 def gen(fr):
@@ -81,7 +89,80 @@ def printAge(post_id):
 def findPerson():
     global videoLocalTime
     global isFindPerson
-    return "%s time: %04d-%02d-%02d %02d:%02d:%02d "%(isFindPerson,videoLocalTime.tm_year, videoLocalTime.tm_mon, videoLocalTime.tm_mday, videoLocalTime.tm_hour,videoLocalTime.tm_min, videoLocalTime.tm_sec)
+    global datalog
+
+    #웹에 보낼 string 문자열
+    datalog = str("%s time: %04d-%02d-%02d %02d:%02d:%02d "%(isFindPerson,videoLocalTime.tm_year, videoLocalTime.tm_mon, videoLocalTime.tm_mday, videoLocalTime.tm_hour,videoLocalTime.tm_min, videoLocalTime.tm_sec))
+   
+    if eq(datalog,"nothing"): # and isFindPerson,"False" 되게 만들기.. 현재는 DB에 False인 값도 들어옴
+        print("감지 못함")
+    else:  
+        timer = threading.Timer(0.7,db_datalog)
+        timer.start()
+        return datalog
+
+#디비 관련 소스함수
+def db_datalog():
+    #print("DB 시작")
+
+    # 접속
+    db = mysql.connector.connect(host='100.100.80.52',port='3306' ,user='bit27', password='123123',database='bit27_db',charset='utf8mb4')
+
+    # 커서 가져오기 (cursor 는 control structure of database 입니다. (연결된 객체라는데 잘모름))
+    cursor = db.cursor()
+
+    print("DB 시작")
+    print(datalog)
+    #데이터 지우기 
+    #add_log = "Delete FROM data"
+
+    #데이터 삽입
+    add_log = "INSERT INTO data (datalog) VALUES ('"+ datalog+"')"
+
+   
+
+    cursor.execute(add_log)
+
+    db.commit()
+
+    cursor.close()
+    db.close()
+
+
+    # SQL 문 만들기
+    #sql = "SELECT * FROM bit27_db.data"
+
+    #----------------------------------------------------------------------------------------------------------
+
+    # SQL 문 만들기
+    #sql = '''
+    #        CREATE TABLE korea2 (
+    #               id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    #               name VARCHAR(20) NOT NULL,
+    #               model_num VARCHAR(10) NOT NULL,
+    #               model_type VARCHAR(10) NOT NULL,
+    #               PRIMARY KEY(id)
+    #        );
+    #    '''
+    #----------------------------------------------------------------------------------------------------------
+
+
+    # 실행하기
+    #cursor.execute(sql)
+ 
+    # DB에 Complete 하기
+    #db.commit()
+    
+    #result = cursor.fetchall()
+
+    #for row_data in result:
+    #    print(row_data[0])
+    #    print(row_data[1])
+    #    print(row_data[2])
+    #    print(row_data[3])  
+    # DB 연결 닫기
+    #db.close()
+
 
 #test
 with app.test_request_context('/hello', method='POST'):
