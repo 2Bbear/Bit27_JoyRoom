0b1707ad93840cad3fcdd60423757e0ac263b1da
2Bbear <wkdwjdgh1234@naver.com>
2018-06-27 20:11:31 +0900

    re
    re

--- MainProgram/HowToUrlDowload/HowToUrlDowload/MyUI.py
+++ MainProgram/HowToUrlDowload/HowToUrlDowload/MyUI.py
@@ -6,23 +6,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-=======
->>>>>>> kim
-=======
-
-
->>>>>>> parent of 90cae96... ui method setting
-=======
-
->>>>>>> parent of 4a21663... DB에서 문자 받고 넣는거 넣음
-=======
-
->>>>>>> parent of 62d1ba7... Merge branch 'kim'
 form_class = uic.loadUiType("0625_DBUI.ui")[0]
 
 class DemoForm(QMainWindow, form_class):
@@ -32,65 +16,51 @@ class DemoForm(QMainWindow, form_class):
         self.setupUi(self)
         
         self.dblistview.setHeaderLabels(["TIME", "LOG"])
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 62d1ba7... Merge branch 'kim'
+
         self.dblistview = self.dblistview.invisibleRootItem()
 
-       # self.SubmitBtn.clicked.connect(self.SubmitbtnEvent)
+        #self.SubmitBtn.clicked.connect(self.SubmitbtnEvent)
         self.okbtn.clicked.connect(self.OkbtnEvent)
         self.cancelbtn.clicked.connect(self.CancelBtnEvent)
         self.checkbtn.clicked.connect(self.CheckBtnEvent)
-<<<<<<< HEAD
+
         self.getlogButton.clicked.connect(self.GetLog)
         #데이터 가져오기
         self.datalog = "nothing"
-<<<<<<< HEAD
-=======
+
         self.dblistview = self.dblistview.invisibleRootItem()       #리스트뷰 설정하는 2줄
 
         self.okbtn.clicked.connect(self.OkbtnEvent)                 #버튼 누르면 지정 텍스트 리스트뷰에 추가
         self.cancelbtn.clicked.connect(self.CancelBtnEvent)         #버튼 누르면 프로그램 종료
         self.Addtargetbtn.clicked.connect(self.AddTargetBtnEvent)         #버튼 누르면 새로운 Dialog창
->>>>>>> kim
-=======
+
         self.dblistview = self.dblistview.invisibleRootItem()
 
        # self.SubmitBtn.clicked.connect(self.SubmitbtnEvent)
         self.okbtn.clicked.connect(self.OkbtnEvent)
         self.cancelbtn.clicked.connect(self.CancelBtnEvent)
         self.checkbtn.clicked.connect(self.CheckBtnEvent)
->>>>>>> parent of 90cae96... ui method setting
-=======
->>>>>>> parent of 4a21663... DB에서 문자 받고 넣는거 넣음
+
 
     #Log url 넣으면 Log 문장을 String으로 반환하는 함수
     def downloadlocaltime(self,url):
         data = urllib.request.urlopen(url).read()
         return str(data)
-=======
 
-    def SubmitbtnEvent(self):
->>>>>>> parent of 62d1ba7... Merge branch 'kim'
+
+
 
     def SubmitbtnEvent(self):
         db = mysql.connector.connect(host='192.168.137.1',port='3306' ,user='bit27_1', password='123123',database='bit27_db',charset='utf8mb4')
         cursor = db.cursor()
-        
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 62d1ba7... Merge branch 'kim'
 
-            add_log = "INSERT INTO datatable (datalog) VALUES ('" +self.datalogchange+ "')"
-            cursor.execute(add_log)
-            db.commit()
-            cursor.close()
-            db.close()
-=======
+
+        add_log = "INSERT INTO datatable (datalog) VALUES ('" +self.datalogchange+ "')"
+        cursor.execute(add_log)
+        db.commit()
+        cursor.close()
+        db.close()
+
         if(self.textbox1.text() != ""):
            submitdata = self.textbox1.text()
            add_log = "INSERT INTO datatable (datalog) VALUES ('"+submitdata+"')"
@@ -98,9 +68,9 @@ class DemoForm(QMainWindow, form_class):
            db.commit()
            cursor.close()
            db.close()
->>>>>>> parent of 4a21663... DB에서 문자 받고 넣는거 넣음
+
         else:
-           return
+            return
 
     def OkbtnEvent(self):
         if(self.textbox1.text() == ""):
@@ -113,39 +83,38 @@ class DemoForm(QMainWindow, form_class):
             item.setText(0, myTime)
             item.setText(1, textforlist)
             self.dblistview.addChild(item)
-<<<<<<< HEAD
+
             self.row = cursor1.fetchone()
 
         cursor1.close()
         db.close()
 
     def DeleteAllofLog(self):
-            # 접속
-            db = mysql.connector.connect(host='192.168.137.1',port='3306' ,user='bit27_1', password='123123',database='bit27_db',charset='utf8mb4')
-            print("시작시작")
-            # 커서 가져오기 (cursor 는 control structure of database 입니다. (연결된 객체라는데 잘모름))
-            cursor = db.cursor()
+        # 접속
+        db = mysql.connector.connect(host='192.168.137.1',port='3306' ,user='bit27_1', password='123123',database='bit27_db',charset='utf8mb4')
+        print("시작시작")
+        # 커서 가져오기 (cursor 는 control structure of database 입니다. (연결된 객체라는데 잘모름))
+        cursor = db.cursor()
         
-            #데이터 지우기 
-            deleteall_log = "DELETE FROM datatable"
+        #데이터 지우기 
+        deleteall_log = "DELETE FROM datatable"
 
-            cursor.execute(deleteall_log)
+        cursor.execute(deleteall_log)
 
+        db.commit()
+
+        cursor.close()
+        db.close()
+
+        if(self.textbox1.text() != ""):
+            submitdata = self.textbox1.text()
+            add_log = "INSERT INTO datatable (datalog) VALUES ('"+submitdata+"')"
+            cursor.execute(add_log)
             db.commit()
-
             cursor.close()
             db.close()
-<<<<<<< HEAD
-=======
-        if(self.textbox1.text() != ""):
-           submitdata = self.textbox1.text()
-           add_log = "INSERT INTO datatable (datalog) VALUES ('"+submitdata+"')"
-           cursor.execute(add_log)
-           db.commit()
-           cursor.close()
-           db.close()
         else:
-           return
+            return
 
     def OkbtnEvent(self):
         if(self.textbox1.text() == ""):
@@ -157,19 +126,14 @@ class DemoForm(QMainWindow, form_class):
             item = QTreeWidgetItem()
             item.setText(0, myTime)
             item.setText(1, textforlist)
-<<<<<<< HEAD
+
             self.dblistview.addChild(item)                                      #집어 넣는데 setText(컬럼번호, 내용) 유의하자
->>>>>>> kim
-=======
+
             self.dblistview.addChild(item)
->>>>>>> parent of 90cae96... ui method setting
-=======
->>>>>>> parent of 4a21663... DB에서 문자 받고 넣는거 넣음
 
     def CancelBtnEvent(self):
         sys.exit()
 
-=======
 
     def CancelBtnEvent(self):
         sys.exit()
@@ -177,10 +141,11 @@ class DemoForm(QMainWindow, form_class):
 
     def CheckBtnEvent(self):
         pass
->>>>>>> parent of 62d1ba7... Merge branch 'kim'
 
     def CheckBtnEvent(self):
         pass
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
