a6d6449be6bf9b1f31d735a7a322f04faca9281b
2Bbear <wkdwjdgh1234@naver.com>
2018-06-27 19:43:52 +0900

    얼구 인식 부분 수정
    얼굴 인식 부분 수정했음

--- CamProgram/HowToWebServer_0621/HowToWebServer_0621/face_recog.py
+++ CamProgram/HowToWebServer_0621/HowToWebServer_0621/face_recog.py
@@ -57,21 +57,23 @@ class FaceRecog():
         frame = self.camera.get_frame()
 
         # Resize frame of video to 1/4 size for faster face recognition processing
+        
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
         
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         self.rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
+        # Get FaceLocation in Frame
+        self.face_locations = face_recognition.face_locations(self.rgb_small_frame,number_of_times_to_upsample=2)
         
         #if self.process_this_frame:
-        if self.currenttimer>10:
+        if self.currenttimer>30:
             # Find all the faces and face encodings in the current frame of video
             
             #t=threading.Thread(target=MyThread,args=(self,))
             #t.start()
-            self.face_locations = face_recognition.face_locations(self.rgb_small_frame)
-            #self.face_encodings = face_recognition.face_encodings(self.rgb_small_frame, self.face_locations)
+            
+            self.face_encodings = face_recognition.face_encodings(self.rgb_small_frame, self.face_locations,num_jitters=0)
             
             self.face_names = []
             for face_encoding in self.face_encodings:
@@ -86,12 +88,12 @@ class FaceRecog():
                 if min_value < 0.6:
                     index = np.argmin(distances)
                     name = self.known_face_names[index]
-                    
-               
-
+                   
+                
+                print(name)
                 self.face_names.append(name)
-                self.currenttimer=0
-
+            self.currenttimer=0
+        
         self.currenttimer=self.currenttimer+1
         self.process_this_frame = not self.process_this_frame
 
@@ -113,23 +115,7 @@ class FaceRecog():
             # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom),  self.targetcolor, 2)
 
-        # Display the results
-        #for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-        #    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        #    top *= 4
-        #    right *= 4
-        #    bottom *= 4
-        #    left *= 4
-
-        #    # Draw a box around the face
-        #    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        #    # Draw a label with a name below the face
-        #    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        #    font = cv2.FONT_HERSHEY_DUPLEX
-        #    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        #    beforName=name
-
+        
         return frame
 
     def get_jpg_bytes(self):
