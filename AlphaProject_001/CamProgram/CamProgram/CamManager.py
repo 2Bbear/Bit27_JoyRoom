de8ae5603e4ff1f7e940d6a351218e733ffa4f9c
2Bbear <wkdwjdgh1234@naver.com>
2018-07-12 20:56:09 +0900

    일단 성공
    성공했따아아아아

---
+++ AlphaProject_001/CamProgram/CamProgram/CamManager.py
@@ -0,0 +1,84 @@
+import threading
+import cv2
+import numpy as np  
+import time
+#Team Libliry
+import WebServer
+import TCPClient
+import Video
+
+#Custom Li
+import Log as l
+
+class CamManger:
+    video=None #Cam 객체
+    thread_video=None
+    thread_makeavi=None
+    thread_webserver=None
+    def __init__(self):
+        l.L_Flow()
+        self.video=Video.Video()
+        pass
+    def __del(self):
+        l.L_Flow()
+        pass
+
+#Override
+    
+#Custom
+    #흐름을 담당하는 함수
+    def Run(self):
+        l.L_Flow()
+
+        #캠 열기
+        self.video.OpenCam(0)
+        #캠 작동 시키기
+        self.thread_video=threading.Thread(target=self.video.RunFrame)
+        self.thread_video.start()
+        time.sleep(1) # 캠이 작동 하는데 까지 약간 시간이 필요함
+        
+        #영상 저장하기
+        self.thread_makeavi=threading.Thread(target=self.MakeAviFile)
+        self.thread_makeavi.start()
+
+        time.sleep(1)
+        #타이머
+        self.SetTimer_A()
+
+
+
+        #Webserver 실행
+        #thread_webserver=threading.Thread(target=WebServer.WebServerStart,args=(self,))
+        #thread_webserver.start()
+        pass
+    
+    #frame 에서 jpg로 변환된 파일을 가져오는 함수
+    def get_jpg_bytes(self):
+        return self.video.get_jpg_bytes()
+    #Cam 영상을 저장하는 함수=================================================
+    def MakeAviFile(self):
+        l.L_Flow()
+        while True:
+            self.video.MakeAviFile()
+        print('waat?')    
+        pass
+    #타이머
+    def SetTimer_A(self):
+        l.L_Flow()
+        tt=threading.Timer(10,self.SetTimer_A)
+        tt.daemon=True
+        tt.start()
+        self.video.ismakeavi=False
+    #===========================================================================
+    #Cam 영상을 화면에 출력하는 함수
+    def ShowCamWindow(self):
+        l.L_Flow()
+        #화면에 출력 시키기
+        print(self.video.frame)
+        while 1:
+            cv2.imshow('dd',self.video.frame)
+            if cv2.waitKey(1)&0xFF == ord('q'):  
+                    return 
+
+
+    
--- AlphaProject_001/CamProgram/CamProgram/Video.py
+++ AlphaProject_001/CamProgram/CamProgram/Video.py
@@ -1,25 +1,78 @@
 #Video.py
 import cv2
 import FaceRecog
+import numpy as np  
+import threading
+import time
 
+lock=threading.Lock()#frame의 안정적인 생성을 위한 락
+
+import Log as l
 class Video:
     frame = None
     camera = None
-    def __init__(self,camnum):
-       self.frame = []
-       self.camera = cv2.VideoCapture(camnum)   
-   #실제 필요한 함수이다.
-    def Run(self):
-                       
-        useface = FaceRecog.FaceRecog()
+    thread_capture=None
+    num=0
+    ismakeavi=True
+    
+    def __init__(self):
+        l.L_Flow()
+        self.frame = []
 
-        ret, self.frame = self.camera.read()                 
-        self.frame = useface.FindFace(self.frame)  
-        
+    #Fram을 jpg 바이너리로 변환하는 함수    
     def get_jpg_bytes(self):
-        self.Run()
+        l.L_Flow()
         ret, jpg = cv2.imencode('.jpg', self.frame)
         return jpg.tobytes()
+    
 
-    def End(self):
+#Custom 
+    #avi영상을 만드는 함수
+    def MakeAviFile(self):
+        l.L_Flow()#ddd
+        try:
+            fps=960
+            width=int(self.camera.get(3))
+            height=int(self.camera.get(4))
+            _saveavifilepath=str(self.num)+'.avi'
+            out = cv2.VideoWriter(_saveavifilepath,cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width,height))
+
+            while(self.ismakeavi):
+                
+                out.write(self.frame)
+        except :
+            pass
+        finally:
+            
+            out.release()
+            self.ismakeavi=True
+            self.num+=1
+        
+    #캠을 여는 함수
+    def OpenCam(self,_camnum):
+        l.L_Flow()
+        self.camera = cv2.VideoCapture(_camnum)  
+        self.camera.set(3,320)
+        self.camera.set(4,240)
+    #프레임을 찍는 함수
+    def CaptureCam(self):
+        
+        lock.acquire()
+        ret,self.frame =self.camera.read()
+        lock.release()
+        
+    #프레임을 계속 찍는 함수
+    def RunFrame(self):
+        l.L_Flow()
+
+        while True:
+            self.thread_capture=threading.Thread(target=self.CaptureCam)
+            self.thread_capture.start()
+            self.thread_capture.join()
+             
         pass
+
+    #캠을 닫는 함수
+    def CloseCam(self):
+        l.L_Flow()
+        self.camera.release()
